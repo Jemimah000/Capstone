4beb1bc890f8337a687973a2c6a7bf84f8b192f2
Jemimah000 <28dora.nobi@gmail.com>
2025-04-17 11:13:12 +0530

    Force redeploy with updated CORS

--- backend/controller/name.js
+++ backend/controller/name.js
@@ -1,15 +1,15 @@
 // controller/name.js
 
 exports.BestFriendName = async (req, res) => {
-  try {
-    const { name } = req.body;
-
-    if (!name) {
-      return res.status(400).json({ message: "Name is required!" });
+    try {
+      const { name } = req.body;
+  
+      if (!name) {
+        return res.status(400).json({ message: "Name is required!" });
+      }
+  
+      res.status(200).json({ message: `Nice ${name}, now onwards you're my best friend! 💖` });
+    } catch (error) {
+      res.status(500).json({ message: "Something went wrong", error });
     }
-
-    res.status(200).json({ message: `Nice ${name}, now onwards you're my best friend! 💖` });
-  } catch (error) {
-    res.status(500).json({ message: "Something went wrong", error });
-  }
-};
+  };
